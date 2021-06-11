af84eea3acd121d53a8792dc4a7c6d990180b484
aker-h <82284797+aker-h@users.noreply.github.com>
2021-06-11 15:33:26 +0900

    Create combination.java

---
+++ recursive/combination.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+/* input format for paiza
+n k               ... k-combination of n 
+*/
+
+public class Main {
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        long num = (long) sc.nextInt();
+        long limit = (long) sc.nextInt();
+        
+        long result = 0;
+        
+        try {
+            result = factorial(num, limit) / factorial(limit, 1);
+            System.out.println(result);
+        } catch (OverflowException e) {
+            System.out.println("OverFlow was occurred.");
+            System.out.println(String.format("Input Number was %d.", num));
+        }
+    }
+    
+    public static long factorial (long num, long limit) throws OverflowException{
+        long result = 1;
+        
+        try {
+            if (num < limit) {
+                return result;
+            } else {
+                result = num * factorial(num - 1, limit);
+            }
+        } catch (OverflowException e) {
+            throw new OverflowException();
+        }
+        
+        if (result < 0) {
+            throw new OverflowException();
+        }
+        
+        return result;
+    }
+}
+
+class OverflowException extends RuntimeException {}
